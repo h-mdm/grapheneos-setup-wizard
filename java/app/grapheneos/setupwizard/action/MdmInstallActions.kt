dd2777983d9c59fd2ec0578a87fc8c4fbd5a83da
mayorow <mayorow@gmail.com>
2025-03-04 13:43:07 +0400

    Fixed the MDM app installation flow after an error

--- java/app/grapheneos/setupwizard/action/MdmInstallActions.kt
+++ java/app/grapheneos/setupwizard/action/MdmInstallActions.kt
@@ -85,6 +85,7 @@ object MdmInstallActions {
             .setMessage(message)
             .setPositiveButton(R.string.button_ok) { dialog, _ ->
                 dialog.dismiss()
+                MdmInstallData.error.postValue(null)
                 context.finish()
             }
             .setCancelable(false)
